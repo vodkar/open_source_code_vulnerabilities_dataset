3148585e7059e60512d8747c059eced0247c3c18
vodkar <boombarah@gmail.com>
2025-01-20 23:45:53 +0500

    first commit

---
+++ src/paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+DATA_PATH = Path("data")
+VULNERABILITY_FIX_COMMITS = DATA_PATH / "vulnerability_fix_commits.csv"
+
+PYTHON_VULNERABILITY_FIXES_DATA_PATH = DATA_PATH / "python_vulnerability_fixes.parquet"
